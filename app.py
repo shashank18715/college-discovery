7583a6288fdb0afa2421c5a8afdb5f9fe77c73d4
shashank18715 <shashank10bca@gmail.com>
2026-09-11 18:44:42 +0530

    redirect view detail to college website

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def college_details(college_id):
     if college is None:
         return "College not found"
 
-    return render_template("colleges.html", college=college)
+    return redirect(college["website"])
 
 
 @app.route("/register", methods=["GET", "POST"])
